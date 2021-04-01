f4497aff126eba0f57ac17eb794a095902764841
Nell Brisard <nell.brisard@coxautoinc.com>
2021-04-01 14:48:21 -0500

    add sample methods

--- modules/aws/dynamodb.go
+++ modules/aws/dynamodb.go
@@ -89,3 +89,22 @@ func GetNellSampleDynamoDBTableE(t testing.TestingT, region string, tableName st
 	}
 	return out.Table, err
 }
+
+// GetContinuousBackupsStatus fetches the dynamoDB tables continuous backup status. This will fail the test if there are any errors.
+func GetContinuousBackupsStatus(t testing.TestingT, region string, tableName string) *DescribeContinuousBackupsOutput {
+	continuousBackupsOutput, err := GetContinuousBackupsStatusE(t, region, tableName)
+	require.NoError(t, err)
+	return continuousBackupsOutput
+}
+
+
+// GetContinuousBackupsStatus fetches the dynamoDB tables continuous backup status.
+func GetContinuousBackupsStatusE(t testing.TestingT, region string, tableName string) (*DescribeContinuousBackupsOutput, error) {
+	out, err := NewDynamoDBClient(t, region).DescribeTable(&DescribeContinuousBackupsInput{
+		TableName: aws.String(tableName),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return out.continuousBackupsOutput, err
+}
